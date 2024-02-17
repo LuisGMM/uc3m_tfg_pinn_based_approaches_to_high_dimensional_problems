d3ad52f34987117d099122fb4df850078309bb65
LuisGMM <69197163+LuisGMM@users.noreply.github.com>
2024-02-17 18:52:24 +0100

    Test high_dimension against random sample

--- src/tfg/nn/high_dimension.py
+++ src/tfg/nn/high_dimension.py
@@ -12,7 +12,7 @@ def f(x: np.ndarray) -> np.ndarray:
     return r * s + np.cos(t) + np.sin(2 * u) + v**3 + np.exp(w)
 
 
-def main():
+def main() -> FFNN:
     def scheduler(opt):
         return LinearLR(
             opt,
@@ -31,16 +31,50 @@ def main():
         num_epochs=5000,
         f=f,
         np_x_train=np_x_train,
-        # batch_size=100,
+        batch_size=100,
         # shuffle=True,
         # scheduler=scheduler,
     )
 
-    # TODO: Test it with some data
+    return ffnn
+
+
+def test_main():
+    ffnn = main()
+
     np_x_test = np.random.uniform(-2, 2, (1000, 6))
     np_y_test = f(np_x_test)
-    print(ffnn(torch.tensor(np_x_test, dtype=torch.float32).to(device)).detach().numpy() ** 2 - np_y_test**2)
+    output = ffnn(torch.tensor(np_x_test, dtype=torch.float32).to(device))
+    loss = torch.nn.MSELoss()(output.flatten(), torch.tensor(np_y_test.flatten(), dtype=torch.float32).to(device))
+    print(f'Loss: {loss.item()}')
+
+
+def plot_over_r6():
+    import matplotlib.pyplot as plt
+
+    np_x_test = np.linspace(-2, 2, 1000)
+    np_x_test = np.array([np_x_test] * 6).T
+    np_y_test = f(np_x_test)
+
+    ffnn = main()
+    output = ffnn(torch.tensor(np_x_test, dtype=torch.float32).to(device))
+
+    plt.title('FFNN vs True Function')
+    plt.grid(axis='both', alpha=0.3)
+    # Remove borders
+    plt.gca().spines['top'].set_alpha(0.0)
+    plt.gca().spines['bottom'].set_alpha(0.3)
+    plt.gca().spines['right'].set_alpha(0.0)
+    plt.gca().spines['left'].set_alpha(0.3)
+
+    plt.xlabel('Line domain in 6D space (r)')
+    plt.ylabel('FFNN(r) and f(r) values')
+
+    plt.plot(np_x_test[:, 0], output.cpu().detach().numpy(), label='Learned Function', color='r')
+    plt.plot(np_x_test[:, 0], np_y_test, label='True Function', linestyle='--', color='b')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    plot_over_r6()
