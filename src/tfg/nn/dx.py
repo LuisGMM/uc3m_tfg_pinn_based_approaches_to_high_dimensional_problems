cdb6fa8b1070a5a7241f1248a525699d591c8c42
LuisGMM <69197163+LuisGMM@users.noreply.github.com>
2024-02-26 14:05:44 +0100

    Update dx.py

--- src/tfg/nn/dx.py
+++ src/tfg/nn/dx.py
@@ -2,5 +2,6 @@
 #
 # du/dx = f
 # input (compute it): f
-# loss: MSE(d(nn)/dx - f)
+# loss: MSE(d(nn)/dx - f) + MSE(nn(a) - u(a)) + MSE(nn(b) - u(b))
 # in some interval [a, b]
+# also add u(a), u(b) to the loss
